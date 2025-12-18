27b0c121d78846061adce8103c7163e651321b80
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2025-12-18 03:03:41 +0100

    Add EF Core DbContext and SQLite configuration

---
+++ GoTogether.Infrastructure/Persistence/GoTogetherDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GoTogether.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTogether.Infrastructure.Persistence
+{
+    public class GoTogetherDbContext : DbContext
+    {
+        public GoTogetherDbContext(DbContextOptions<GoTogetherDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<EventInterest> EventInterests => Set<EventInterest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
