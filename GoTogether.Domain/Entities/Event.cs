d4a6ae0ba982b9d45cf1e0f0f4e977c255f5c4c0
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2025-12-22 00:05:16 +0100

    Changed Event Description to non-nullable

--- GoTogether.Domain/Entities/Event.cs
+++ GoTogether.Domain/Entities/Event.cs
@@ -2,12 +2,12 @@
 
 namespace GoTogether.Domain.Entities
 {
-    public class Event(string Title, DateTime StartsAt, string Location, string? Description) : Entity
+    public class Event(string Title, DateTime StartsAt, string Location, string Description) : Entity
     {
         public string Title { get; set; } = Title;
         public DateTime StartsAt { get; set; } = StartsAt;
         public string Location { get; set; } = Location;
-        public string? Description { get; set; } = Description;
+        public string Description { get; set; } = Description;
 
         public ICollection<EventInterest> EventInterests { get; private set; } = [];
     }
